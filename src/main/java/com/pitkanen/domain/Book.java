77d498eb57bb043913c79a231dbef872c4f35f2b
Pitkänen Juha <jwpitkanen@gmail.com>
2017-02-16 16:13:10 +0200

    Added REST-api, endpoints for returning all books and a single book by ID

--- src/main/java/com/pitkanen/domain/Book.java
+++ src/main/java/com/pitkanen/domain/Book.java
@@ -3,10 +3,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
--- src/main/java/com/pitkanen/web/BookController.java
+++ src/main/java/com/pitkanen/web/BookController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.pitkanen.domain.Book;
 import com.pitkanen.service.BookRepository;
 
@@ -53,5 +55,16 @@ public class BookController{
 		
 		return "login";
 	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId){
+		return (Book) repository.findOne(bookId);
+	
+}
 		
 }
